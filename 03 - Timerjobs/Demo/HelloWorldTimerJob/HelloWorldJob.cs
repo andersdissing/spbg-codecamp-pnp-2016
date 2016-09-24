a53ef8dde896ef2b74c5b23023c0ac45a1442a9b
Bernd Rickenberg <bernd@rickenberg.dk>
2016-09-24 20:11:30 +0200

    Demo code for remote timer job added.

---
+++ 03 - Timerjobs/Demo/HelloWorldTimerJob/HelloWorldJob.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OfficeDevPnP.Core.Framework.TimerJobs;
+
+namespace HelloWorldTimerJob
+{
+    public class HelloWorldJob : TimerJob
+    {
+        public HelloWorldJob() : base("HelloWorldJob")
+        {
+            TimerJobRun += (sender, args) =>
+            {
+                try
+                {
+                    var context = args.WebClientContext;
+                    context.Load(context.Web, w => w.Title);
+                    context.ExecuteQuery();
+                    var title = context.Web.Title;
+                    Console.WriteLine($"Site title: {title}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error has occured: {ex.Message}");
+                }
+            };
+        }
+    }
+}
---
+++ 03 - Timerjobs/Demo/HelloWorldTimerJob/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldTimerJob
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var job = new HelloWorldJob();
+            job.UseOffice365Authentication("bernd@rickenberg.net", GetPassword());
+            job.AddSite("https://rickenberg.sharepoint.com/codecamp2016");
+            job.Run();
+        }
+
+        private static string GetPassword()
+        {
+            Console.WriteLine("Input your password: ");
+            var result = string.Empty;
+            ConsoleKeyInfo keyInfo;
+            do
+            {
+                keyInfo = Console.ReadKey(true);
+                result += keyInfo.KeyChar;
+                Console.Write("*");
+            } while (keyInfo.Key != ConsoleKey.Enter);
+            Console.WriteLine();
+            return result;
+        }
+    }
+}
